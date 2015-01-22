9099c539a5eb6c3d0a479ec52ff2125d3114ad61
jdoppler <joerg.doppler@tuwien.ac.at>
2015-01-22 14:20:34 -0200

    initial commit

---
+++ ep/dissipation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2.7
+
+import numpy as np
+
+
+class Loss(i, j, xn=0, yn=0, sigmax=0, sigmay=0):
+    """Position dependent loss class.
+
+    Returns a function which places Gaussian dissipation profiles at coordinates
+    {xn,yn}.
+    """
+    
+
