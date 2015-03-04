969e359019eb96296abfd9e7a325eb3553c01980
jdoppler <joerg.doppler@tuwien.ac.at>
2015-03-04 21:42:28 +0100

    feature: output left-right flipped imaginary part in 4th column

--- ep/potential.py
+++ ep/potential.py
@@ -208,7 +208,7 @@ def write_potential(N=2.5, pphw=20, amplitude=1.0, sigmax=1e-1, sigmay=1e-1,
         np.savetxt("potential_real.dat", zip(range(len(real_vector)), real_vector),
                    fmt=["%i", "%.12f"])
         np.savetxt("potential_imag_xy.dat",
-                   zip(X.flatten('F'), Y.flatten('F'), imag_vector))
+                   zip(X.flatten('F'), Y.flatten('F'), imag_vector, np.fliplr(p.imag).flatten('F')))
 
     if shape == 'RAP':
         x = p.WG.t
