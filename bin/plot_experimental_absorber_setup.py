7d65e3b6634a81ec9bd192f062efa0b191095460
jdoppler <joerg.doppler@tuwien.ac.at>
2016-02-06 15:41:58 +0100

    added thesholds for configuration #1

--- bin/plot_experimental_absorber_setup.py
+++ bin/plot_experimental_absorber_setup.py
@@ -57,8 +57,8 @@ def main(W=0.05, L=25, config=1, phase=None, plot=False, threshold_left=None, th
             threshold_right = 0.0
         elif config == 1:
             phase = -np.pi
-            threshold_left = 0.0
-            threshold_right = 0.0
+            threshold_left = 0.225
+            threshold_right = 1.26
         elif config == 2:
             phase = np.pi
             threshold_left = 0.31
@@ -143,8 +143,8 @@ def main(W=0.05, L=25, config=1, phase=None, plot=False, threshold_left=None, th
     ax5.plot(x[piece_mask], a - xi_periodic[piece_mask], "y-")
 
     periodic_absorber = np.concatenate([a, a[::-1], a, a[::-1], a, a[::-1], a, a[::-1], a])
-    # if config == 3:
-    # periodic_absorber = np.concatenate([a, a[::-1], a, a[::-1], a, a[::-1], a, a[::-1], a, a[::-1], a, a[::-1], a])
+    if config == 1:
+        periodic_absorber = np.concatenate([a, a[::-1], a, a[::-1], a, a[::-1], a, a[::-1], a, a[::-1], a, a[::-1], a])
 
     elements = len(periodic_absorber)/len(a)
     x_elements = np.linspace(x0 - elements*dx,
